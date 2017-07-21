cd141bcb1a665bd2dc3c6450a84bacb2b4f11d64
Praveen <praveen.mahadoria@afourtech.com>
2017-07-21 18:06:17 +0530

    updated

--- DemoTest/src/com/demoaut/newtours/farmework/jenkins/JenkinsRESTClient.java
+++ DemoTest/src/com/demoaut/newtours/farmework/jenkins/JenkinsRESTClient.java
@@ -1,5 +1,18 @@
 package com.demoaut.newtours.farmework.jenkins;
 
+import java.io.IOException;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+
 public class JenkinsRESTClient 
 {
 	private String schema;
@@ -9,6 +22,7 @@ public class JenkinsRESTClient
 	private String password;
 
 	private String baseUrl;
+	private CloseableHttpClient httpClient;
 	
 	public JenkinsRESTClient(String schema, String hostName, int port, String user, String password) 
 	{
@@ -17,6 +31,76 @@ public class JenkinsRESTClient
 		this.port = port;
 		this.user = user;
 		this.password = password;
-		this.baseUrl = this.schema + ":\\" + hostName + ":" + port;
+		this.baseUrl = this.schema + ":\\\\" + hostName + ":" + port;
  	}
+	
+	private String getBaseUrl()
+	{
+		return baseUrl;
+	}
+	
+	public void postXMLData(String endpoint, String data) throws IOException
+	{
+		String uri = getBaseUrl() + endpoint;
+
+		System.out.println("Posting xml data to: " + uri);
+
+		CloseableHttpResponse response;
+
+		HttpPost post = new HttpPost(uri);
+
+		post.setHeader("Content-type", "text/xml; charset=ISO-8859-1");
+		post.setEntity(new StringEntity(data));
+
+		response = getHttpClient().execute(post);
+
+		System.out.println("Request status line: " + response.getStatusLine());
+
+		try
+		{
+			response.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public CloseableHttpClient getHttpClient()
+	{
+		if(httpClient == null)
+		{
+			HttpClientBuilder builder = HttpClients.custom();
+
+			/*if(schema.equals("https"))
+			{
+				SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+				SSLConnectionSocketFactory sslConnectionSocketFactory = null;
+				try
+				{
+					sslContextBuilder.loadTrustMaterial(KeyStore.getInstance(KeyStore.getDefaultType()), new TrustSelfSignedStrategy());
+					sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContextBuilder.build(), SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+
+				builder.setSSLSocketFactory(sslConnectionSocketFactory);
+			}*/
+
+			if(user != null && password != null)
+			{
+				CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+				credentialsProvider.setCredentials(new AuthScope(hostname, 8443), new UsernamePasswordCredentials(user, password));
+
+				builder.setDefaultCredentialsProvider(credentialsProvider);
+			}
+
+			httpClient = builder.build();
+		}
+
+		return httpClient;
+	}
 }
--- DemoTest/src/com/demoaut/newtours/utilities/jenkins/SuitesManagementTask.java
+++ DemoTest/src/com/demoaut/newtours/utilities/jenkins/SuitesManagementTask.java
@@ -1,6 +1,84 @@
 package com.demoaut.newtours.utilities.jenkins;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import com.demoaut.newtours.farmework.jenkins.JenkinsRESTClient;
+
 public class SuitesManagementTask 
 {
+	public static String currentDir = System.getProperty("user.dir");
 	
+	@Test
+	public void createRegressionSuite()
+	{
+		try
+		{
+			String suiteConfigFilePath = currentDir+ "\\src\\com\\demoaut\\newtours\\resources\\suite1.json";
+			
+			String content = getResourceContent(suiteConfigFilePath);
+			
+			JSONObject suiteConfiguration = new JSONObject(content);
+			
+			String suiteName = suiteConfiguration.getString("name");
+			
+			JenkinsRESTClient client = new JenkinsRESTClient("http", "localhost", 8080, null, null);
+
+			String suiteViewTemplate = getResourceContent(currentDir+ "\\src\\com\\demoaut\\newtours\\resources\\view_config_template.xml");
+
+			String suiteViewConfig = suiteViewTemplate
+					.replace("[view_name]", suiteName)
+					.replace("[include_jobs_regex]", suiteName + ".*");
+			
+			client.postXMLData(getCreateViewEndpoint(suiteName), suiteViewConfig);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getResourceContent(String name)
+	{
+		
+		String content = "";
+
+		try
+		{
+			File file = new File(name);
+			content = FileUtils.readFileToString(file);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			throw new RuntimeException("Could not read the content of the file '" + name + "'.");
+		}
+
+		return content;
+	}
+	
+	public static String getCreateViewEndpoint(String jobName)
+	{
+		String encodedJobName;
+		try
+		{
+			encodedJobName = URLEncoder.encode(jobName, "UTF-8").replaceAll("\\+", "%20");
+		}
+		catch(UnsupportedEncodingException e)
+		{
+			e.printStackTrace();
+			throw new RuntimeException("Can not connect to Jenkins. See the Exception stacktrace in the console output.");
+		}
+
+		return "\\createView?name=" + encodedJobName;
+	}
 }
